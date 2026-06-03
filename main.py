cdf444d1b5e93cc21db175eb4062ab70ead8e418
bhavya sachula <bhavyasachula07@gmail.com>
2026-06-03 22:34:07 +0530

    Getting started with fastapi

---
+++ main.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI
+from models import Products
+app = FastAPI()
+
+
+@app.get("/")
+def greet():
+    return {"message":"Hello Sorcerer supreme"}
+
+products= [
+    Products(id=1,name="phone",description="An iphone",price=99,quantity=10),
+    Products(id=4,name="keyboard",description="An keyboard",price=99,quantity=10),
+    Products(id=5,name="mouse",description="A mouse",price=99,quantity=10),
+    Products(id=6,name="bag",description="A bag",price=99,quantity=10)
+]
+@app.get("/products")
+def getallproducts():
+    return products
+
+@app.get("/product/{id}")
+def get_product_by_id(id:int):
+    for product in products:
+        if product.id == id:
+            return product
+      
+    return {"message":"product not found"}
---
+++ models.py
@@ -0,0 +1,8 @@
+from dataclasses import dataclass
+@dataclass
+class Products():
+    id:int
+    name:str
+    description:str
+    price:float
+    quantity:int
