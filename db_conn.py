634ba5d251c5b37d62352631a13c3b26fed5b410
bhavya sachula <bhavyasachula07@gmail.com>
2026-06-04 19:44:04 +0530

    Adding the existing product list as sqlalchemy object

--- db_conn.py
+++ db_conn.py
@@ -4,3 +4,4 @@ from sqlalchemy import create_engine
 url = "postgresql://postgres:bhavya@localhost:5432/MyProjectDb"
 engine = create_engine(url)
 session = sessionmaker(autoflush=False,autocommit=False,bind=engine)
+# session is the main connection object 
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from models import Products
-from db import session,engine
+from db_conn import session,engine
 import database_models  
 
 app = FastAPI()
@@ -17,60 +17,62 @@ data ={
 }
 def introduce(firstname,lastname):
     print(f"my name is {firstname}-{lastname}")
-    
-introduce(**data)
 
-# products= [
-#     Products(id=1,name="phone",description="An iphone",price=99,quantity=10),
-#     Products(id=4,name="keyboard",description="An keyboard",price=99,quantity=10),
-#     Products(id=8,name="mouse",description="A mouse",price=99,quantity=10),
-#     Products(id=6,name="bag",description="A bag",price=99,quantity=10)
-# ]
+#want to check just uncomment this 
+# Unpacking the key value pairs
+# introduce(**data)
 
-# database_models.Base.metadata.create_all(bind=engine)
+products= [
+    Products(id=1,name="phone",description="An iphone",price=99,quantity=10),
+    Products(id=4,name="keyboard",description="An keyboard",price=99,quantity=10),
+    Products(id=8,name="mouse",description="A mouse",price=99,quantity=10),
+    Products(id=6,name="bag",description="A bag",price=99,quantity=10)
+]
 
-# def init_db():
-#     db=session()
-#     for product in products:
-#         print(**product.model_dump())
-#         # print(database_models.Product())
+database_models.Base.metadata.create_all(bind=engine)
+
+def init_db():
+    db=session()
+    for product in products:
+        # print(product.model_dump())
+        db.add(database_models.Product(**product.model_dump()))
         
-# init_db()
+init_db()
 
-# @app.get("/products")
-# def getallproducts():
-#     #database connection 
-#     db = session()
-#     db.query()
-#     return products
+@app.get("/products")
+def getallproducts():
+    #database connection 
+    db = session()
+    db.query()
+    return products
 
-# @app.get("/product/{id}")
-# def get_product_by_id(id:int):
-#     for product in products:
-#         if product.id == id:
-#             return product
+@app.get("/product/{id}")
+def get_product_by_id(id:int):
+    for product in products:
+        if product.id == id:
+            return product
       
-#     return {"message":"product not found"}
+    return {"message":"product not found"}
 
-# @app.post("/product")
-# def add_product(product:Products):
-#     products.append(product)
+@app.post("/product")
+def add_product(product:Products):
+    products.append(product)
 
 
     
-# @app.delete("/product/{id}")
-# def remove_product(id:int):
-#     for i in range(len(products)):
-#       if products[i].id == id:
-#         del products[i]
-#         return {"removed the product"}
+@app.delete("/product/{id}")
+def remove_product(id:int):
+    for i in range(len(products)):
+      if products[i].id == id:
+        del products[i]
+        return {"removed the product"}
         
-#     return "product is not found"
+    return "product is not found"
 
-# @app.put("/product")
-# def update_product(id:int , product:Products):
-#     for i in range(len(products)):
-#             if products[i].id == id:
-#                 products[i] = product 
-#                 return {"updated product":product}
-#     return {"product not found"}
+@app.put("/product")
+def update_product(id:int , product:Products):
+    for i in range(len(products)):
+            if products[i].id == id:
+                products[i] = product 
+                return {"updated product":product}
+    return {"product not found"}
